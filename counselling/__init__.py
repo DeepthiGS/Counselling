79dbe87663e1f216e279001141af4405eaf969d6
DeepthiGS <65117457+DeepthiGS@users.noreply.github.com>
2020-10-15 16:24:52 +0530

    Changed the uri

--- counselling/__init__.py
+++ counselling/__init__.py
@@ -13,7 +13,7 @@ local_server = False
 if local_server:
     app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12aer56uil90@localhost/counsell"
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ksjwoswolbgdtk:cce092f6b7fba675ae2f51f7cf2a27b2536f90f4099f724c3c713bde17758ee6@ec2-52-72-34-184.compute-1.amazonaws.com:5432/dff0ekedh9f848"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://crttszlcbtfnte:1d78c500a199e2394e98c220b94311fcbc869ff3f681bb3c65f243370e9f3b3b@ec2-54-160-18-230.compute-1.amazonaws.com:5432/du3u2mt8n1fdc"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
